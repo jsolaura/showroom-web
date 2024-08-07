ddd563607d9b6717d9610b3b2cd9c89a444ec27f
jsolaura <jke@peopulley.com>
2024-08-07 14:27:17 +0900

    각 콘텐츠별 title, description 정리 data

---
+++ constans/data.ts
@@ -0,0 +1,44 @@
+interface IContent {
+    title: string;
+    description: string;
+    images: string[];
+    color: string;
+}
+export const data: IContent[] = [
+    {
+        title: '시원한 느낌을 주는 오버뷰',
+        description: '입장과 동시에 탁 트인 통창이 따사로운 햇빛으로 반겨주는 것을 확인할 수 있는데요.\n 채광이 잘 드는 통창 너머로는 서울 서대문구 연희동의 평화로운 일상이 펼쳐집니다.\n 따뜻하고 친근한 인상을 줌과 동시에, DiiVER의 시원한 컬러감과 카펫이 시선을 사로잡는데요.\n 푸른 카펫은 청량함을 더해주며 한정된 공간을 더욱 넓게 보이는 효과까지 기대할 수 있습니다.',
+        images: ['content1.jpeg',],
+        color: 'blue',
+    },
+    {
+        title: '고객과 만남이 시작되는 다이빙대',
+        description: '푸른 바다(카펫)가 DiiVER를 상징한다면 그곳에 누구나 흠뻑 빠져들수록 도와주는 것이 있습니다.\n 바로 다이빙대를 닮은 미팅 테이블인데요. 성공적인 미팅을 이루어 우리의 Buddy가 되기 바라는 소망과 메시지가 숨어있죠.\n 센스 있는 스토리텔링에 누구든  DiiVE DEEP TOGETHER 할 수 밖에 없을 겁니다.',
+        images: ['content2.jpeg',],
+        color: 'skyBlue',
+    },
+    {
+        title: 'DiiVER의 대표 굿즈를 만나는 포트폴리오 존',
+        description: '홈페이지에 있던 *포트폴리오*(고객사, PB 상품 등)를 직접 육안으로 확인 가능한 포트폴리오 존도 마련되어 있습니다.\n 쇼룸에 방문해 주신 고객님들은 누구나 DiiVER가 제작한 다양한 굿즈와 샘플을 자유롭게 구경할 수 있고,\n 실물을 마주함에 따라 재질 등 제작 컨디션에 있어 직접적으로 선택을 빠르게 도울 수 있죠.\n 함께 미팅하는 고객사들이 멋진 포트폴리오가 되어 이곳을 구성하기 바라는 DiiVER의 기대와 포부가 숨어있기도 합니다.',
+        images: ['content3_1.jpeg', 'content3_2.jpeg'],
+        color: 'white',
+    },
+    {
+        title: '자유롭게 아이디어를 나눌 수 있는 네트워킹 존',
+        description: 'DiiVER의 직원들을 위한 네트워킹 존도 마련되어 있습니다.\n 이곳은 DiiVER의 가족이라면 누구든 자유롭게 이용 가능한 오피스 공간으로,\n 각 자리에는 모니터가 마련되어 있어 조금 더 자유로운 분위기 속에서 효율적으로 업무를 진행할 수 있어요.\n 우리만의 아이디어를 조금 더 아늑한 공간에서 나누고 싶다면 내부 회의실을 이용할 수 있습니다.\n 물론 고객사도 이용 가능하니 방문을 희망하는 경우 편하게 문의주셔도 좋아요.',
+        images: ['content4_1.jpeg', 'content4_2.jpeg'],
+        color: 'skyBlue',
+    },
+    {
+        title: '방문 고객에게 제공되는 기프트',
+        description: '쇼룸에서는 이미지에 있는 기프트 외에도 다양한 방문 기프트를 받을 수 있습니다.\n 요즘 키링은 정말 많이들 달고 다니잖아요. 어디에서 볼 수 없던 DiiVER만의 심플하고 유니크한 키링, 직접 가차를 돌려 획득하면 어떨까요?',
+        images: ['content5_1.jpeg', 'content5_2.jpeg', 'content5_3.jpeg'],
+        color: 'white',
+    },
+    {
+        title: '입퇴장도 DiiVER스럽게',
+        description: '쇼룸에 방문하는 순간 이색적인 분위기를 느낄 수 있도록 문까지 신경을 썼습니다.\n 마치 애니메이션 스폰지밥에 등장하는 집게리아가 연상되는 귀여운 창문과 문고리.\n 입장부터 퇴장까지 쇼룸에서의 경험이 문이라는 매개체를 통해 더욱 오래, 그리고 특별하게 기억될 거에요.',
+        images: ['content6_1.jpeg', 'content6_2.jpeg'],
+        color: 'blue',
+    },
+]
